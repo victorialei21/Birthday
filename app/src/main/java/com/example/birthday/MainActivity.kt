f69c98ac26850db35103b4196bebd5552fa121d9
victorialei21 <58520925+victorialei21@users.noreply.github.com>
2022-02-04 13:05:35 -0500

    added all activities and menu

--- app/src/main/java/com/example/birthday/MainActivity.kt
+++ app/src/main/java/com/example/birthday/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.birthday
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }//onCreate
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater: MenuInflater = menuInflater
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }//onCreateOptionsMenu
+
+    fun selectGame(view : View){
+
     }
-}
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.pinata -> intent = Intent(this, PinataActivity::class.java)
+            R.id.candles -> intent = Intent(this, CandlesActivity::class.java)
+            R.id.gift -> intent = Intent(this, GiftActivity::class.java)
+        }
+        startActivity(intent)
+        return false
+    } //end onOptionsItemSelected
+
+}//MainActivity
