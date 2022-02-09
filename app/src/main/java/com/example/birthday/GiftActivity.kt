ac110cddcee023eb26da771a5535c35a65b6b05c
mitzyun <mqy2@georgetown.edu>
2022-02-08 21:33:59 -0500

    gifts game complete

--- app/src/main/java/com/example/birthday/GiftActivity.kt
+++ app/src/main/java/com/example/birthday/GiftActivity.kt
@@ -1,11 +1,63 @@
 package com.example.birthday
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.ImageButton
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+
 
 class GiftActivity : AppCompatActivity() {
+    var presentNdx = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_gift)
-    }
-}
+        presentNdx = (1..9).shuffled().first()
+        //Toast.makeText(applicationContext, presentNdx.toString(), Toast.LENGTH_LONG).show()
+
+    } // onCreate
+
+    fun disappear (view : View) {
+        val b = view as ImageButton
+        val ourId = b.resources.getResourceEntryName(b.id)
+        val presentButton = "button$presentNdx"
+
+        b.animate().alpha(0f).rotation(720f).scaleX(0.5F).scaleY(0.5F).duration = 2000
+
+        // end game and reveal present
+        if (ourId==presentButton) {
+            val handler = Handler()
+            handler.postDelayed(Runnable {
+                b.setImageResource(R.drawable.present)
+                b.animate().alpha(1f).rotation(720f).scaleX(1F).scaleY(1F).duration = 500
+            }, 1500)
+            handler.postDelayed(Runnable {
+                endgame()
+            }, 2500)
+        }
+    } // disappear
+
+    private fun endgame () {
+        AlertDialog.Builder(this)
+            .setIcon(android.R.drawable.ic_dialog_alert)
+            .setTitle("You found the present!")
+            .setPositiveButton("  Play Again", DialogInterface.OnClickListener()
+            {   dialogInterface: DialogInterface?, i: Int ->
+                // reset page
+                this.recreate()
+            })
+            .setNegativeButton("Return to Main   ", DialogInterface.OnClickListener()
+            {   dialogInterface: DialogInterface?, i: Int ->
+                // return to main menu
+                startActivity(Intent(this, MainActivity::class.java))
+            })
+            .show()
+
+    } // endgame
+
+} // GiftActivity
