6e744ab5397afdbada6e38605436a4b179c2b4b9
victorialei21 <58520925+victorialei21@users.noreply.github.com>
2022-02-09 00:03:32 -0500

    candles!

--- app/src/main/java/com/example/birthday/CandlesActivity.kt
+++ app/src/main/java/com/example/birthday/CandlesActivity.kt
@@ -2,16 +2,17 @@ package com.example.birthday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 
 class CandlesActivity : AppCompatActivity() {
     var age = 1
-    lateinit var cake : ImageView
+    lateinit var cake : ImageButton
     lateinit var ageText : TextView
-    lateinit var candle1 : ImageView
-    lateinit var candle2 : ImageView
-    lateinit var candle3 : ImageView
+    lateinit var candles : ImageButton
+    lateinit var candlesBlownOut : ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +22,17 @@ class CandlesActivity : AppCompatActivity() {
         age = intent.getIntExtra("Age", 1)
         cake = findViewById(R.id.cakeImage)
         ageText = findViewById(R.id.ageText)
-        candle1 = findViewById(R.id.candle1)
-        candle2 = findViewById(R.id.candle2)
-        candle3 = findViewById(R.id.candle3)
+        candles = findViewById(R.id.candlesImage)
+        candlesBlownOut = findViewById(R.id.candlesBlownImage)
 
         ageText.text = age.toString()
 
     }//onCreate
+
+    fun blowFlame(view : View){
+        candles!!.visibility = View.INVISIBLE
+        candlesBlownOut!!.visibility = View.VISIBLE
+
+    }
 
 }//CandlesActivity
